2111f296fb1415c59d816942785aeed806bbdefc
vbobroff <vbobrov.code@outlook.com>
2020-07-23 21:39:37 +0700

    Set AssemblyInfo

--- VectorTextBlock/Properties/AssemblyInfo.cs
+++ VectorTextBlock/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("VectorTextBlock")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Positioning vector text characters along a curved line")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Victor Bobrov")]
 [assembly: AssemblyProduct("VectorTextBlock")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("VBobroff.NET")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
@@ -42,5 +42,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
